492ee4676dc76e4dbadf1282ccbcb8cbb1cbcb21
Atralupus <me@atralupus.com>
2025-04-07 16:16:14 +0900

    kst

--- NodeUptime/BlockTests/BlockTimestampTests.cs
+++ NodeUptime/BlockTests/BlockTimestampTests.cs
@@ -45,7 +45,7 @@ namespace NodeUptime.BlockTests
                     var blockTimestamp = DateTime.Parse(
                         response.ChainQuery.BlockQuery.Blocks[0].Timestamp
                     );
-                    var currentTime = DateTime.UtcNow;
+                    var currentTime = DateTime.UtcNow.AddHours(9); // KST = UTC+9
 
                     var timeDifference = (currentTime - blockTimestamp).TotalSeconds;
 
