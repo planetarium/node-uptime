82e9b43931ce43a4ec5f59b6c9dc7f5439311625
sky1045 <ghdwlgud4545@gmail.com>
2025-06-05 10:10:30 +0900

    Remove odin rpc1 for volume reset

--- NodeUptime/Constants.cs
+++ NodeUptime/Constants.cs
@@ -10,7 +10,7 @@ namespace NodeUptime
             new ReadOnlyDictionary<string, string>(
                 new Dictionary<string, string>
                 {
-                    { "OdinRpc1", "https://odin-rpc-1.nine-chronicles.com/graphql" },
+                    // { "OdinRpc1", "https://odin-rpc-1.nine-chronicles.com/graphql" },
                     { "OdinRpc2", "https://odin-rpc-2.nine-chronicles.com/graphql" },
                     { "OdinRpc3", "https://odin-rpc-3.nine-chronicles.com/graphql" },
                     { "HeimdallRpc1", "https://heimdall-rpc-1.nine-chronicles.com/graphql" },
