e8735faf6ac86ec380584a16fe0bec0f88ecb91d
Robert <35055316+sky1045@users.noreply.github.com>
2025-06-19 20:05:58 +0900

    reset node volume

--- NodeUptime/Constants.cs
+++ NodeUptime/Constants.cs
@@ -11,8 +11,8 @@ namespace NodeUptime
                 new Dictionary<string, string>
                 {
                     { "OdinRpc1", "https://odin-rpc-1.nine-chronicles.com/graphql" },
-                    { "OdinRpc2", "https://odin-rpc-2.nine-chronicles.com/graphql" },
-                    { "OdinRpc3", "https://odin-rpc-3.nine-chronicles.com/graphql" },
+                    // { "OdinRpc2", "https://odin-rpc-2.nine-chronicles.com/graphql" },
+                    // { "OdinRpc3", "https://odin-rpc-3.nine-chronicles.com/graphql" },
                     { "HeimdallRpc1", "https://heimdall-rpc-1.nine-chronicles.com/graphql" },
                     { "HeimdallRpc2", "https://heimdall-rpc-2.nine-chronicles.com/graphql" },
                     { "OdinValidator5", "https://odin-validator-5.nine-chronicles.com/graphql" },
