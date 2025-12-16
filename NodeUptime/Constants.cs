9f25cc227e1f80d6f7d1eeacaf0e89c07e0d1d3e
eseiker <285361+eseiker@users.noreply.github.com>
2025-12-16 15:49:39 +0900

    update Constants.cs
    added thor validator and removed unused headless urls (fetched via planet registry)

--- NodeUptime/Constants.cs
+++ NodeUptime/Constants.cs
@@ -10,24 +10,8 @@ namespace NodeUptime
             new Dictionary<string, string[]>
             {
                 { "odin", new[] { "https://odin-validator-5.nine-chronicles.com/graphql" } },
-                { "heimdall", new[] { "https://heimdall-validator-1.nine-chronicles.com/graphql" } }
+                { "heimdall", new[] { "https://heimdall-validator-1.nine-chronicles.com/graphql" } },
+                { "thor", new[] { "https://thor-validator-1.nine-chronicles.com/graphql" } }
             };
-
-        public static readonly IReadOnlyDictionary<string, string> HEADLESS_URLS =
-            new ReadOnlyDictionary<string, string>(
-                new Dictionary<string, string>
-                {
-                    { "OdinRpc1", "https://odin-rpc-1.nine-chronicles.com/graphql" },
-                    { "OdinRpc2", "https://odin-rpc-2.nine-chronicles.com/graphql" },
-                    { "OdinRpc3", "https://odin-rpc-3.nine-chronicles.com/graphql" },
-                    { "HeimdallRpc1", "https://heimdall-rpc-1.nine-chronicles.com/graphql" },
-                    { "HeimdallRpc2", "https://heimdall-rpc-2.nine-chronicles.com/graphql" },
-                    { "OdinValidator5", "https://odin-validator-5.nine-chronicles.com/graphql" },
-                    {
-                        "HeimdallValidator1",
-                        "https://heimdall-validator-1.nine-chronicles.com/graphql"
-                    },
-                }
-            );
     }
 }
